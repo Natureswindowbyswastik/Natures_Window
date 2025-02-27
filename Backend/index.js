9c520f54cf2d166b8ce17fd05851ad108f3cab04
Aakanshya1 <aakanshya.gyawali@gmail.com>
2025-02-28 01:36:03 +0545

    Added Vercel config and updated files

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,7 @@ const FormRouter = require('./routes/FormRouter')
 const GalleryRouter = require("./routes/GalleryRouter");
 const BlogRouter = require('./routes/BlogRouter')
 const AuthRouter  = require('./routes/AuthRouter')
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -17,6 +18,7 @@ app.use('/blog',BlogRouter);
 app.use('/auth',AuthRouter);
 app.use('/form',FormRouter);
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on Port ${process.env.PORT}`);
-})
+const port = process.env.PORT || 3001; // Default to 3001 for local development
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+});
